6fc797697db4fe78c8dc4b430a6e48c26f469227
Sharklet <ywzsunny@buaa.edu.cn>
2019-08-16 01:06:01 +0800

    update

---
+++ 全国大学生电子设计大赛相关/模块驱动/int2bin.py
@@ -0,0 +1,15 @@
+
+def int2bin(a,bytes):
+    m = int(bin(a)[2:])
+    if bytes==1:
+        return "%08d" % m
+    elif bytes == 2:
+        return "%016d" % m
+    elif bytes == 3:
+        return "%024d" % m
+    elif bytes == 4:
+        return "%032d" % m
+
+def bin2int(a):
+    m = "0b" + a
+    return (int)(m)
