8eedbf01a3d2fc0a23d7b1df29c8d3ea4596f9a5
Sharklet <ywzsunny@buaa.edu.cn>
2019-09-05 23:05:13 +0800

    update

---
+++ web相关/本地ini文件当数据库/config.py
@@ -0,0 +1,20 @@
+import json,os
+class config():
+    def __init__(self,fileName):
+        self.fileName = fileName
+        pass
+    def readConfig(self,key):
+        #if self.fileName in os.listdir():
+        try:
+            f = open(self.fileName,'r') 
+            data = json.loads(f.read())  
+            value = data.get(key)       
+            f.close()                  
+            return value                
+        except:
+            return 0
+    def writeConfig(self,dict):
+        f = open(self.fileName,'w')     
+        data = json.dumps(dict)       
+        f.write(data)                   
+        f.close()                  
