77b63940226fa5c19ad58b64064cc0d5c8caaf74
Sharklet <ywzsunny@buaa.edu.cn>
2019-10-10 10:16:47 +0800

    update

--- 智能软硬件 (python全栈系列)/Micropython_esp32/bluetooth(demo）/ble.py
+++ 智能软硬件 (python全栈系列)/Micropython_esp32/bluetooth(demo）/ble.py
@@ -1,15 +1,22 @@
 import bluetooth
-bt = bluetooth.Bluetooth()
-bt.active(1)
-bt.advertise(100, 'MicroPython')
-print('----')
-tx = bluetooth.Characteristic('6E400002-B5A3-F393-E0A9-E50E24DCCA9E', bluetooth.FLAG_READ|bluetooth.FLAG_NOTIFY)
-rx = bluetooth.Characteristic('6E400003-B5A3-F393-E0A9-E50E24DCCA9E', bluetooth.FLAG_WRITE)
-s = bt.add_service('6E400001-B5A3-F393-E0A9-E50E24DCCA9E', [tx, rx])
-tx.write('foo')
+
 
 def callback(char, data):
     print('on update data:', data)
     
-rx.on_update(callback)
+class BLE():
+    def __init__(self):
+        self.bt = bluetooth.Bluetooth()
+        self.bt.active(1)
+        self.bt.advertise(100, 'MicroPython')
+        print('----')
+        self.tx = bluetooth.Characteristic('6E400002-B5A3-F393-E0A9-E50E24DCCA9E', bluetooth.FLAG_READ|bluetooth.FLAG_NOTIFY)
+        self.rx = bluetooth.Characteristic('6E400003-B5A3-F393-E0A9-E50E24DCCA9E', bluetooth.FLAG_WRITE)
+        self.s = self.bt.add_service('6E400001-B5A3-F393-E0A9-E50E24DCCA9E', [self.tx, self.rx])
+       
+        self.rx.on_update(callback)
 
+
+
+    def write(self,text): #str类型传入
+        self.tx.write(text)
